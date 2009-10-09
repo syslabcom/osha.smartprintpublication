f4eed5431e532a3eaa0ef62f0356d6b65d854cc4
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-09 15:24:48 +0000

    added basic functionality. It is now possible to create a File based on the PDF- output

--- osha/smartprintpublication/browser/publication_settings.py
+++ osha/smartprintpublication/browser/publication_settings.py
@@ -4,11 +4,12 @@ from zope.component import adapts
 from zope.app.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
 from zope.formlib import form
-from DateTime import DateTime
 from persistent import Persistent
 from zope.annotation import factory
 from Products.ATContentTypes.interface.document import IATDocument
 from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
+import Acquisition
+import transaction
 
 from osha.smartprintpublication.interfaces import IOshaSmartprintSettings
 from osha.theme import OSHAMessageFactory as _
@@ -40,12 +41,39 @@ class OshaSmartprintSettingsForm(form.PageEditForm):
 
     @form.action(_("Apply"))
     def handle_edit_action(self, action, data):
-        print "data:", data
+        
         settings = IOshaSmartprintSettings(self.context)
         settings.path = data['path']
         settings.issue = data['issue']
+        
+        asPDF = self.context.restrictedTraverse('asPDF', None)
+        if not asPDF:
+            # sth bad has happened
+            print "view asPDF not found"
+            return
+        rawPDF = asPDF(number=data['issue'], plainfile=True)
 
-        status = _("Updated on ${date_time}",
-                   mapping={'date_time': DateTime()}
-                   )
-        self.status = status
+        path = data['path']
+        if path.startswith('/'):
+            path = path[1:]
+        dest = self.context.restrictedTraverse(path, None)
+        filename = "%s.pdf" %self.context.getId()
+        if not dest:
+            print "destination folder not found!"
+            return
+
+        verb ="Updated"
+        if not getattr(Acquisition.aq_base(dest), filename, None):
+            dest.invokeFactory(type_name="File", id=filename)
+            transaction.savepoint()
+            verb="Added"
+        newFile = getattr(dest, filename)
+        newFile.unmarkCreationFlag()
+
+        newFile.processForm(values=dict(id=filename, title=self.context.Title()))
+        newFile.setFile(rawPDF)
+        
+        self.status = u"%(verb)s publication at %(url)s" %dict(verb=verb, url=newFile.absolute_url())
+        # del newFile
+        
+        
