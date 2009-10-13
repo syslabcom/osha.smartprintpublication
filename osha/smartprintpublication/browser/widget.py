e2471e8b63f5bb83e88144a803ab638296dcf159
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-13 12:47:51 +0000

    added a datepicker widget

--- osha/smartprintpublication/browser/widget.py
+++ osha/smartprintpublication/browser/widget.py
@@ -4,6 +4,9 @@ from zope.app.form.browser.widget import SimpleInputWidget
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from types import StringType, UnicodeType
+import datetime
+import time
+from Products.CMFPlone.i18nl10n import monthname_english
 
 class ReferenceURLWidget(SimpleInputWidget):
 
@@ -38,3 +41,49 @@ class ReferenceURLWidget(SimpleInputWidget):
     def hasInput(self):
         return (self.name + '.marker') in self.request.form
 
+
+class DatePickerWidget(SimpleInputWidget):
+
+    __call__ = ViewPageTemplateFile('datepickerwidget.pt')
+
+    def _getFormInput(self):
+        year = self.request.get(self.name + '_year')
+        try:
+            year = int(year)
+        except:
+            return None
+        month = self.request.get(self.name + '_month')
+        try:
+            month = int(month)
+        except:
+            return None
+        day = self.request.get(self.name + '_day')
+        try:
+            day = int(day)
+        except:
+            return None
+        #import pdb; pdb.set_trace()
+        return datetime.date(year, month, day)
+
+    def getYearRange(self):
+        year = datetime.date.fromtimestamp(time.time()).year
+        return range(year-7, year+2)
+
+    def getMonthRange(self):
+        return [dict(val=i, name=monthname_english(i)) for i in range(1,13)]
+
+    def getYear(self):
+        #return ""
+        return isinstance(self._data, datetime.date) and self._data.year or ''
+    
+    def getMonth(self):
+        #return ""
+        return isinstance(self._data, datetime.date) and self._data.month or ''
+    
+    def getDay(self):
+        #return ""
+        return isinstance(self._data, datetime.date) and self._data.day or ''
+
+
+    def hasInput(self):
+        return (self.name + '.marker') in self.request.form
