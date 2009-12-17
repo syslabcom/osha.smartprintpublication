211a9df12df7cf112511d3e9fa56ebb2edb6b1bf
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-12-17 15:16:33 +0000

    Reformatted the "also available online" box for publications that were created onlin according to Gorka's wishes

--- osha/smartprintpublication/browser/adapters.py
+++ osha/smartprintpublication/browser/adapters.py
@@ -1,6 +1,7 @@
 from zope.interface import implements
 from zope.annotation.interfaces import IAnnotations
 from Products.CMFCore.utils import getToolByName
+from Products.PlacelessTranslationService import getTranslationService
 
 from osha.smartprintpublication.config import PUBLICATION_DOCUMENT_REFERENCE
 
@@ -18,7 +19,31 @@ class AdditionalPublicationInfo(object):
             if res:
                 ob = res[0].getObject()
                 if ob:
-                    return "<div class='publicationLanguageBox'><a href='%(url)s'>" \
-                        "Also available as HTML</a></div>" %dict(url=ob.absolute_url())
+                    portal_languages = getToolByName(self.context, 'portal_languages')
+                    preflang = portal_languages.getPreferredLanguage()
+                    ali = portal_languages.getAvailableLanguageInformation()
+                    
+                    translations = ob.getTranslations()
+                    lang_codes = translations.keys()
+                    lang_codes.sort()
+                    links = list()
+                    for lang in lang_codes:
+                        trans = translations[lang][0]
+                        url = trans.absolute_url()
+
+                        name = ali.get(lang, {'native': lang})['native']
+                        links.append( (name, url) )
+                    
+                    pts = getTranslationService()
+                    label = pts.translate(domain="osha",
+                        msgid=u'label_also_online',
+                        context=self.context,
+                        target_language=preflang,
+                        default=u'Also available online')
+                    text = """<div class='publicationLanguageBox'>%(label)s: """ %dict(label=label)
+                    for link in links:
+                        text +="""[<a href="%(url)s">%(name)s</a>] """ %dict(name=link[0], url=link[1])
+                    text += "</div>"
+                    return text
         return u""
 
