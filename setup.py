b1a45d1ee3311193557dfc3e472cc9044e648627
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-01-27 17:42:33 +0000

    Back to development: 0.5.1dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.5'
+version = '0.5.1dev'
 
 long_description = (
     read('README.txt')
